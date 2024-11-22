43c2a98f4bff06d13a904caefe675ed376f6ddb8
JackieLin2004 <linxy20040304@163.com>
2024-11-22 23:14:34 +0800

    Project Configuration

---
+++ src/test/java/ynu/mediinsight/MediInsightBackend/MediInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ynu.mediinsight.MediInsightBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediInsightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
