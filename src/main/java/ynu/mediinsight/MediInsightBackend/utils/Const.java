cf1d73831360b7248459705336afdcdfcb015548
JackieLin2004 <linxy20040304@163.com>
2024-11-23 20:14:54 +0800

    Complete cross-domain and login functionality

--- src/main/java/ynu/mediinsight/MediInsightBackend/utils/Const.java
+++ src/main/java/ynu/mediinsight/MediInsightBackend/utils/Const.java
@@ -6,4 +6,5 @@ package ynu.mediinsight.MediInsightBackend.utils;
  */
 public class Const {
     public static final String JWT_BLACK_LIST = "jwt:blacklist:";
+    public static final int ORDER_CORS = -102;
 }
